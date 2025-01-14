f83050d726b6e56180057248990c33973b8e6119
aspizu <aspizu@protonmail.com>
2025-01-14 13:18:03 +0530

    Add route to serve index.html for improved static file handling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import {staticPlugin} from "@elysiajs/static"
 import {swagger} from "@elysiajs/swagger"
-import {Elysia} from "elysia"
+import {Elysia, file} from "elysia"
 
 const app = new Elysia()
     .use(swagger())
     .use(staticPlugin({indexHTML: true}))
+    .get("/", () => file("./public/index.html"))
     .listen(3000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
