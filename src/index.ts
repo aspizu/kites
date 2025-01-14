63450a8c7601026852137ab42938125d49fda9a2
aspizu <aspizu@protonmail.com>
2025-01-14 13:16:35 +0530

    Enhance static plugin configuration and add index.html for improved user experience

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import {Elysia} from "elysia"
 
 const app = new Elysia()
     .use(swagger())
-    .use(staticPlugin())
-    .get("/", () => "Hello Elysia")
+    .use(staticPlugin({indexHTML: true}))
     .listen(3000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
